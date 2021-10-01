6c5b955df3227680e7766f011551ebfeaba4734f
Robert Fuke <rfuke@loffacorp.com>
2021-10-01 02:16:29 -0700

    add form in contactUs page
     Committer: Robert Fuke <rfuke@loffacorp.com>
     On branch feature/Robert
     Changes to be committed:
    	modified:   client/src/App.js
    	modified:   client/src/components/Nav/index.js

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -14,7 +14,7 @@ function Nav() {
             </Link>
           </li>
           <li className="mx-1">
-            <Link to="/contactUs">
+            <Link to="/ContactUs">
               Contract Us
             </Link>
           </li>
