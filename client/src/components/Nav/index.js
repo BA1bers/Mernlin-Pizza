e3d964b4ea588102bb88341b0890310747ac1731
BA1bers <brent.albers@yahoo.com>
2021-10-02 13:18:26 -0700

    fixed some css styling issues

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,6 +8,7 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
+import './style.css'
 
 function Nav() {
 
