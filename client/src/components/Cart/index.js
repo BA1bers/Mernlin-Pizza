3a6ae48aaf8ae9d9850ddf626a33a42d9fbb5667
Fule <laki.linux@gmail.com>
2021-10-04 12:45:35 -0700

    calculate discount on total

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/react-hooks';
-import { QUERY_CHECKOUT } from "../../utils/queries"
-import { idbPromise } from "../../utils/helpers"
-import CartItem from "../CartItem";
-import Auth from "../../utils/auth";
-import OrderButton from '../Images/orderNowButton.png'
-import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import "./style.css";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import OrderButton from '../Images/orderNowButton.png';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import './style.css';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_USER } from '../../utils/queries';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
@@ -19,8 +21,8 @@ const Cart = () => {
   useEffect(() => {
     if (data) {
       stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session })
-      })
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
     }
   }, [data]);
 
@@ -28,7 +30,7 @@ const Cart = () => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-    };
+    }
 
     if (!state.cart.length) {
       getCart();
@@ -41,8 +43,8 @@ const Cart = () => {
 
   function calculateTotal() {
     let sum = 0;
-    state.cart.forEach(item => {
-      sum += item.price * item.purchaseQuantity;
+    state.cart.forEach((item) => {
+      sum += (item.price / 100) * 80 * item.purchaseQuantity;
     });
     return sum.toFixed(2);
   }
@@ -57,7 +59,7 @@ const Cart = () => {
     });
 
     getCheckout({
-      variables: { products: productIds }
+      variables: { products: productIds },
     });
   }
 
@@ -71,36 +73,32 @@ const Cart = () => {
 
   return (
     <div className="cart">
-      <div className="close" onClick={toggleCart}>[close]</div>
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
       <h2>Shopping Cart</h2>
       {state.cart.length ? (
         <div>
-          {state.cart.map(item => (
+          {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
 
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
 
-            {
-              Auth.loggedIn() ?
-                <button onClick={submitCheckout}>
-                  Checkout
-              </button>
-                :
-                <div>
-                <button onClick={submitCheckout}>
-                  Checkout
-              </button>
-                <span>(Log in to get 20% discount)</span></div>
-            }
+            {Auth.loggedIn() ? (
+              <button onClick={submitCheckout}>Checkout</button>
+            ) : (
+              <div>
+                <button onClick={submitCheckout}>Checkout</button>
+                <span>(SignUp to get 20% discount)</span>
+              </div>
+            )}
           </div>
         </div>
       ) : (
-          <h3>
-          Your cart is empty
-          </h3>
-        )}
+        <h3>Your cart is empty</h3>
+      )}
     </div>
   );
 };
