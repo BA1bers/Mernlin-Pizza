001f93a3ff602cc5c68cb8ac1402524110ab1d0a
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-10-04 15:25:56 -0700

    added herocontainer

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Hero() {
+  return (
+      <HeroContainer>
+        <h1>MERNlin's Pizza</h1>
+        
+      </HeroContainer>
+      
+  )
+}
+
+export default Hero;
