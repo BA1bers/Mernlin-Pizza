f586db879ddc3485cb6756c694ce11f6913e6d58
Bryan Daniel <brdaniel117@gmail.com>
2021-10-04 21:25:28 -0700

    about us page working with routes

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -4,9 +4,35 @@ function AboutGroup() {
 
     return (
         <div className="container">
-            <section>
-                <dev>Hellllo</dev>
-            </section>
+            <div>
+                <img></img>
+                <img></img>
+                <h2>Paul Mernlin</h2>
+                <h3>Founder and Owner</h3>
+                <p>Paul Mernlin the owner of MERNlin’s Pizza wanted to bring his store into a new stage by adding in digital online ordering website application. MERNlin’s Pizza was started in 2000 and has one historic location in downtown Scottsdale. Family own sense day one, we include everyone into our pizza family.</p>
+            </div>
+            <div>
+                <div>
+                    <img></img>
+                    <h3>Brent Albers</h3>
+                    <h4>Website Co-Designer Frontend</h4></div>
+                    <p>TBD</p>
+                <div>
+                    <img></img>
+                    <h3>Bryan Daniel</h3>
+                    <h4>Website Co-Designer Backend</h4></div>
+                    <p>TBD</p>
+                <div>
+                    <img></img>
+                    <h3>Bryce Sitler</h3>
+                    <h4>Website Co-Designer Frontend</h4></div>
+                    <p>TBD</p>
+                <div>
+                    <img></img>
+                    <h3>Robert Fluke</h3>
+                    <h4>Website Co-Designer Backend</h4></div>
+                    <p>TBD</p>
+            </div>
         </div>
     );
 }
