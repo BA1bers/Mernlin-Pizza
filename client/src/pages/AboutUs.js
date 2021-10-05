dd2541fba6dacec146e8a1e91fe54e8af8ea9485
Bryan Daniel <brdaniel117@gmail.com>
2021-10-04 20:47:18 -0700

    removed about us front end

---
+++ client/src/pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutList from "../components/AboutUs";
+
+
+const AboutUs = () => {
+
+
+    return (
+        <div className="container backgroundImage">
+            {console.log(AboutList)}
+            TESTSTESTTESST
+        </div>
+    );
+};
+
+export default AboutUs;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,18 +3,22 @@ import ProductList from "../components/ProductList";
 import Cart from "../components/Cart";
 import { useStoreContext } from '../utils/GlobalState';
 import Jumbotron from "../components/Jumbotron";
+import AboutUs from "../components/AboutUs/index"
 
 const Home = () => {
 
   const [state, dispatch] = useStoreContext();
 
   return (
-    <div className="container backgroundImage">
+    <><div className="container backgroundImage">
       {console.log(state)}
-      {state.currentCategory === ""? <Jumbotron />:<ProductList /> }
+      {state.currentCategory === "" ? <Jumbotron /> : <ProductList />}
 
       <Cart />
     </div>
+    <div>
+        {console.log(AboutUs)}
+      </div></>
   );
 };
 
