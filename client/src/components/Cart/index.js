c4a21d606debf53d5f7196786d4ea79a2d402200
BA1bers <brent.albers@yahoo.com>
2021-10-06 18:15:05 -0700

    worked more on resizing nav components

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -88,7 +88,7 @@ const Cart = () => {
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
-        <img src={OrderButton} alt="OrderButton" />
+        <img className="orderButton" src={OrderButton} alt="OrderButton" />
       </div>
     );
   }
